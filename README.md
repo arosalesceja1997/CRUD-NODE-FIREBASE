eccc871f3d69bc5bd50386c8c991d3703a9687d9
Anthony Tonatihu Rosales Ceja <anthony@vetta.io>
2020-03-20 17:18:31 -0700

    Delete and Add

---
+++ README.md
@@ -0,0 +1,1 @@
+export GOOGLE_APPLICATION_CREDENTIALS="/home/anthony/Proyectos/NODE/CRUD-NODE-FIREBASE/crud-node-95ccb-firebase-adminsdk-qgv6s-5188657f64.json"
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,38 @@
 const { Router } = require('express');
 const router = Router();
+const admin = require('firebase-admin');
+
+const serviceAccount = require("../../crud-node-95ccb-firebase-adminsdk-qgv6s-5188657f64.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL :'https://crud-node-95ccb.firebaseio.com/'
+});
+
+const db = admin.database();
 
 router.get('/', (req, res) => {
-	res.render('index.hbs');
+	db.ref('contacts').once('value', (snapshot) => {
+		const data = snapshot.val();
+		res.render('index.hbs', {contacts : data});
+	});
+});
+
+router.post('/new-contact', (req, res) => {
+	const newContact = {
+		firstname: req.body.firstname,
+		lastname: req.body.lastname,
+		email: req.body.email,
+		phone: req.body.phone
+	};
+
+	db.ref('contacts').push(newContact);
+	res.redirect('/');
+});
+
+router.get('/delete-contact/:id', (req, res) => {
+	db.ref('contacts/' + req.params.id).remove();
+	res.redirect('/');
 });
 
 module.exports = router;
